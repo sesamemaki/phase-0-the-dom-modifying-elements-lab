d5065e4b2791e56cd3778e802afb82479f7058e5
Burcu <burju7@yahoo.com>
2023-04-20 12:56:25 +0300

    completed assingment

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Write your code here!
+const main = document.querySelector("#main")
+main.remove()
+
+const newHeader = document.createElement("h1")
+newHeader.textContent = "Header"
+newHeader.setAttribute("id", "victory");
+
+const name = "Burcu"
+newHeader.innerHTML = `${name} is the champion`
+
+const body = document.querySelector("body")
+body.appendChild(newHeader)
